7a4385fb82b67c345d7709b56136cce08ce93965
Rick Ramirez <Rick.ramirez6174@gmail.com>
2021-03-31 00:05:51 -0700

    Added separate activities for age, ethnicity, and gender. Need to update mainActivity to transition between them.

---
+++ app/src/main/java/com/bignerdranch/android/covidapp/Age.kt
@@ -0,0 +1,27 @@
+package com.bignerdranch.android.covidapp
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+
+class Age : AppCompatActivity() {
+    var age = 0;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_age)
+
+        // pull up keyboard to collect ino
+        fun showSoftKeyboard(view: View) {
+            if (view.requestFocus()) {
+                val imm: InputMethodManager =
+                    getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
+            }
+        }
+        //
+        //Need to use the numberpad to collect age for editText
+    }
+}
---
+++ app/src/main/java/com/bignerdranch/android/covidapp/Ethnicity.kt
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.covidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ethnicity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ethnicity)
+    }
+}
